aed0e537a8f22fb154a18249e05782fc8b90ba4e
Peter Jenkin <peternjenkin@gmail.com>
2019-04-05 18:49:25 +0100

    9-66 Delegates added for tableView, and subclassing of ViewController, (with line of code to check that table rendering ok)

--- Landmark image book/ViewController.swift
+++ Landmark image book/ViewController.swift
@@ -8,17 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    // NB manually subclass/inherit ViewController also from UITableViewDelegate, UITableViewDataSource
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        // self so as to have more options when subclassing tableview functions - eg numberOfRowsInSection and cellForRowAt
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+// delegate functions for table - see subclassing of ViewController
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10       // tell table to have 10 rows (count 'em)
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()    // instantiate a cell
+        cell.textLabel?.text = "Checking cell code" // check rendering ok
+        return cell     // this cell for table row(s)
+    }
+
+   // override func didReceiveMemoryWarning() {
+     //   super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    //}
 
 
 }
