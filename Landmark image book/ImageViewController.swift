93aad62dfec9e4135dbfc62d425157447206da50
Peter Jenkin <peternjenkin@gmail.com>
2019-04-05 19:42:37 +0100

    9-69 Another ViewController added, with an ImageView, and a navigation pane added to both ViewControllers

---
+++ Landmark image book/ImageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ImageViewController.swift
+//  Landmark image book
+//
+//  Created by Peter Jenkin on 05/04/2019.
+//  Copyright © 2019 Peter Jenkin. All rights reserved.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
