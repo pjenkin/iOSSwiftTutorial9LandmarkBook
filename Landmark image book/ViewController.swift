61c2dfe05900d32d478f9979e168cc28da42b789
Peter Jenkin <peternjenkin@gmail.com>
2019-04-05 18:22:01 +0100

    Initial commit

---
+++ Landmark image book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Landmark image book
+//
+//  Created by Peter Jenkin on 05/04/2019.
+//  Copyright © 2019 Peter Jenkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
